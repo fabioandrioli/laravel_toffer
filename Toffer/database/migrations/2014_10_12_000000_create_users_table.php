87c337cc9595855e3642515c3ab1943b5c0145f3
Fabio Gilberto <fabio.goncalves@bomjesus.br>
2022-01-06 22:46:27 -0300

    Arrumação do cadastro

--- Toffer/database/migrations/2014_10_12_000000_create_users_table.php
+++ Toffer/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('surname');
+            $table->string('status');
+            $table->string('status_cadastro');
             $table->string('phone');
             $table->string('cpf')->nullable();
             $table->string('street_name')->nullable();
